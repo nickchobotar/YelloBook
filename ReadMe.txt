50e9ef8feccc40d8fb559e5d41a51f969d821e46
Nick Chobotar <nick.chobotar@outlook.com>
2017-12-23 22:37:40 -0500

    d

---
+++ ReadMe.txt
@@ -0,0 +1,22 @@
+The "Yellow Book C Sharp Programming Course" by Rob Miles 2013-2014
+
+These are the slide decks, lab sessions and coursework for the First Year Programming course taught at Hull University in semester 2013-2014. The content is organised as follows:
+
+Lectures - a set of lecture slide decks delivered each week
+Labs - a set of exercises keyed to the taught content
+ACW - two large deliverables, ACW1 to be delivered at the end of Week 18 and ACW2 to be delivered at the end of Week 40. An marking scheme and marking procedure is included.
+
+The week numbering is according to the University of Hull calendar for that year, which means that the first week is numbered week 11. 
+
+All content was written and delivered by Rob Miles (www.robmiles.com)
+
+They are free for use in any teaching or revision exercises on condition that they are not resold and that the name Rob Miles remains on the texts.
+
+The C# Yellow Book, the companion text for the programming course is available at www.csharpcourse.com
+
+Feel free to send any comments on the content to comments@csharpcourse.com
+
+Have fun
+
+Rob Miles
+February 2015
---
+++ Yellow Book Code Samples 6.4/Code Sample 25 Printing Account values from an array/YellowBookSamples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+enum AccountState
+{
+    New,
+    Active,
+    UnderAudit,
+    Frozen,
+    Closed
+} ;
+
+  //A structure is a collection of C# variables which you want to 
+    //    treat as a single entity. In C# a lump of data would 
+    //    be called a structure and each part of it would be called a field.
+
+struct Account  // Structure
+{
+    public AccountState State;  
+    public string Name;   // these are fields
+    public string Address;
+    public int AccountNumber;
+    public int Balance;
+    public int Overdraft;
+} ;
+
+class BankProgram
+{
+
+    public static void PrintAccount(Account a)
+    {
+        Console.WriteLine("Name: " + a.Name);
+        Console.WriteLine("Address: " + a.Address);
+        Console.WriteLine("Balance: " + a.Balance);
+    }
+
+    public static void Main()
+    {
+        const int MAX_CUST = 100;
+        Account[] Bank = new Account[MAX_CUST];
+        Bank[0].Name = "Rob";
+        Bank[0].Address = "Robs House";
+        Bank[0].State = AccountState.Active;
+        Bank[0].Balance = 1000000;
+        
+	PrintAccount(Bank[0]);
+	        
+	Bank[1].Name = "Jim";
+        Bank[1].Address = "Jim's House";
+        Bank[1].State = AccountState.Frozen;
+        Bank[1].Balance = 0;
+        
+	PrintAccount(Bank[1]);
+    }
+}
